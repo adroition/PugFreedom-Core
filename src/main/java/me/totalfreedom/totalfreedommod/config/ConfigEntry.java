23e3cdd39b5b40e774d6fbc643020f04b4a297d6
rovertdude <tsannoyingorange@gmail.com>
2016-06-09 21:36:10 -0500

    Update ConfigEntry.java

--- src/main/java/me/totalfreedom/totalfreedommod/config/ConfigEntry.java
+++ src/main/java/me/totalfreedom/totalfreedommod/config/ConfigEntry.java
@@ -35,6 +35,8 @@ public enum ConfigEntry
     SERVER_ADDRESS(String.class, "server.address"),
     SERVER_MOTD(String.class, "server.motd"),
     SERVER_OWNERS(List.class, "server.owners"),
+    SERVER_MANAGERS(List.class, "server.managers"),
+    SERVER_COOWNERS(List.class, "server.coowners"),
     SERVER_BAN_URL(String.class, "server.ban_url"),
     SERVER_PERMBAN_URL(String.class, "server.permban_url"),
     //
