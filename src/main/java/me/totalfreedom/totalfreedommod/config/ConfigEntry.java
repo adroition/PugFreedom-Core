3dde715c5ff4fdbc62055712307403bd5a851856
rovertdude <tsannoyingorange@gmail.com>
2016-06-11 18:02:22 -0500

    Update ConfigEntry.java

--- src/main/java/me/totalfreedom/totalfreedommod/config/ConfigEntry.java
+++ src/main/java/me/totalfreedom/totalfreedommod/config/ConfigEntry.java
@@ -86,6 +86,7 @@ public enum ConfigEntry
     OVERLORD_IPS(List.class, "overlord_ips"),
     NOADMIN_IPS(List.class, "noadmin_ips"),
     ADMIN_ONLY_MODE(Boolean.class, "admin_only_mode"),
+    SENIOR_ONLY_MODE(Boolean.class, "senior_only_mode"),
     AUTO_ENTITY_WIPE(Boolean.class, "auto_wipe");
     //
     private final Class<?> type;
